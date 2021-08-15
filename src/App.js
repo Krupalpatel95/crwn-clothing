9a70b5d585e3fa15e032b356afe4b857383a0472
Krupal Patel <krupalpatel95@gmail.com>
2021-08-15 10:35:08 +0530

    update menu-items to navigate withRoute higher order component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import './App.css';
+
 
 function App() {
   return (
-    <Homepage></Homepage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage}></Route>
+        <Route path='/shop/hats' component={HatsPage}></Route>
+      </Switch>
+    </div>
+
   );
 }
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 export default App;
